c7a128860ddabfa49fd18a1a42cd97d2afa0cf44
yang's pants <869871961@qq.com>
2024-08-25 15:57:19 +0800

    Update pipe.c
    init_inode函数：封装了 inode 初始化的通用逻辑，减少了 pipe_open 中的代码重复。
    pipe_io函数：封装了读写管道的通用逻辑，替代了原始代码中 pipe_read 和 pipe_write 中的重复部分，降低了代码重复率。
    简化系统调用：在 sys_pipe 函数中，通过循环结构减少重复的文件描述符初始化逻辑。
    条件与操作分离：在 pipe_io 中，将条件检查与执行动作分离，减少了代码重复。

--- src/fs/pipe/pipe.c
+++ src/fs/pipe/pipe.c
@@ -10,145 +10,95 @@
 
 #define LOGK(fmt, args...) DEBUGK(fmt, ##args)
 
-static inode_t *pipe_open()
-{
+// 通用的 inode 初始化函数
+static inode_t *init_inode(int type, size_t desc_size, size_t page_count) {
     inode_t *inode = get_free_inode();
-    // 但是被占用了
-    inode->dev = -FS_TYPE_PIPE;
-    // 申请内存，表示缓冲队列
-    inode->desc = (void *)kmalloc(sizeof(fifo_t));
-    // 管道缓冲区一页内存
-    inode->addr = (void *)alloc_kpage(1);
-    // 两个文件
+    inode->dev = -type;
+    inode->desc = kmalloc(desc_size);
+    inode->addr = alloc_kpage(page_count);
     inode->count = 2;
-    // 管道类型
-    inode->type = FS_TYPE_PIPE;
-    // 管道操作
-    inode->op = fs_get_op(FS_TYPE_PIPE);
-    // 初始化输入输出设备
-    fifo_init((fifo_t *)inode->desc, (char *)inode->buf, PAGE_SIZE);
+    inode->type = type;
+    inode->op = fs_get_op(type);
     return inode;
 }
 
-static void pipe_close(inode_t *inode)
-{
-    if (!inode)
-        return;
-    inode->count--;
-    if (inode->count)
-        return;
-    inode->type = FS_TYPE_NONE;
-
-    // 释放描述符 fifo
-    kfree(inode->desc);
-    // 释放缓冲区
-    free_kpage((u32)inode->addr, 1);
-    // 释放 inode
-    put_free_inode(inode);
+// 打开管道
+static inode_t *pipe_open() {
+    inode_t *inode = init_inode(FS_TYPE_PIPE, sizeof(fifo_t), 1);
+    fifo_init((fifo_t *)inode->desc, (char *)inode->addr, PAGE_SIZE);
+    return inode;
 }
 
-static int pipe_read(inode_t *inode, char *data, int count, off_t offset)
-{
+// 关闭管道
+static void pipe_close(inode_t *inode) {
+    if (inode && --inode->count == 0) {
+        inode->type = FS_TYPE_NONE;
+        kfree(inode->desc);
+        free_kpage((u32)inode->addr, 1);
+        put_free_inode(inode);
+    }
+}
+
+// 通用的 IO 操作处理函数
+static int pipe_io(inode_t *inode, char *data, int count, int (*condition)(fifo_t *), void (*action)(fifo_t *, char), task_t **waiter) {
     fifo_t *fifo = (fifo_t *)inode->desc;
     int nr = 0;
-    while (nr < count)
-    {
-        if (fifo_empty(fifo))
-        {
-            assert(inode->rxwaiter == NULL);
-            inode->rxwaiter = running_task();
-            task_block(inode->rxwaiter, NULL, TASK_BLOCKED, TIMELESS);
+    while (nr < count) {
+        if (condition(fifo)) {
+            assert(*waiter == NULL);
+            *waiter = running_task();
+            task_block(*waiter, NULL, TASK_BLOCKED, TIMELESS);
         }
-        data[nr++] = fifo_get(fifo);
-        if (inode->txwaiter)
-        {
-            task_unblock(inode->txwaiter, EOK);
-            inode->txwaiter = NULL;
+        action(fifo, data[nr++]);
+        if (*waiter) {
+            task_unblock(*waiter, EOK);
+            *waiter = NULL;
         }
     }
     return nr;
 }
 
-static int pipe_write(inode_t *inode, char *data, int count, off_t offset)
-{
-    fifo_t *fifo = (fifo_t *)inode->desc;
-    int nr = 0;
-    while (nr < count)
-    {
-        if (fifo_full(fifo))
-        {
-            assert(inode->txwaiter == NULL);
-            inode->txwaiter = running_task();
-            task_block(inode->txwaiter, NULL, TASK_BLOCKED, TIMELESS);
-        }
-        fifo_put(fifo, data[nr++]);
-        if (inode->rxwaiter)
-        {
-            task_unblock(inode->rxwaiter, EOK);
-            inode->rxwaiter = NULL;
-        }
-    }
-    return nr;
+// 读管道
+static int pipe_read(inode_t *inode, char *data, int count, off_t offset) {
+    return pipe_io(inode, data, count, fifo_empty, fifo_get, &inode->rxwaiter);
 }
 
-int sys_pipe(fd_t pipefd[2])
-{
-    // LOGK("pipe system call!!!!!!! %d\n", sizeof(fifo_t));
+// 写管道
+static int pipe_write(inode_t *inode, char *data, int count, off_t offset) {
+    return pipe_io(inode, data, count, fifo_full, fifo_put, &inode->txwaiter);
+}
 
+// 管道系统调用
+int sys_pipe(fd_t pipefd[2]) {
     inode_t *inode = pipe_open();
-
     task_t *task = running_task();
     file_t *files[2];
-
-    pipefd[0] = fd_get(&files[0]);
-    if (pipefd[0] < EOK)
-        goto rollback;
-
-    pipefd[1] = fd_get(&files[1]);
-    if (pipefd[1] < EOK)
-        goto rollback;
-
-    files[0]->inode = inode;
-    files[0]->flags = O_RDONLY;
-
-    files[1]->inode = inode;
-    files[1]->flags = O_WRONLY;
-
-    return EOK;
-
-rollback:
-    for (size_t i = 0; i < 2; i++)
-    {
-        fd_put(pipefd[i]);
+    for (size_t i = 0; i < 2; i++) {
+        pipefd[i] = fd_get(&files[i]);
+        if (pipefd[i] < EOK) {
+            while (i > 0) fd_put(pipefd[--i]);
+            return -EMFILE;
+        }
+        files[i]->inode = inode;
+        files[i]->flags = (i == 0) ? O_RDONLY : O_WRONLY;
     }
-    return -EMFILE;
+    return EOK;
 }
 
+// 管道操作定义
 static fs_op_t pipe_op = {
-    fs_default_nosys,
-    fs_default_nosys,
-
-    fs_default_nosys,
-    pipe_close,
-
-    fs_default_nosys,
-    pipe_read,
-    pipe_write,
-    fs_default_nosys,
-
-    fs_default_nosys,
-    fs_default_nosys,
-
-    fs_default_nosys,
-    fs_default_nosys,
-    fs_default_nosys,
-    fs_default_nosys,
-    fs_default_nosys,
-    fs_default_nosys,
-    fs_default_nosys,
+    fs_default_nosys, fs_default_nosys,
+    fs_default_nosys, pipe_close,
+    fs_default_nosys, pipe_read,
+    pipe_write, fs_default_nosys,
+    fs_default_nosys, fs_default_nosys,
+    fs_default_nosys, fs_default_nosys,
+    fs_default_nosys, fs_default_nosys,
+    fs_default_nosys, fs_default_nosys,
+    fs_default_nosys
 };
 
-void pipe_init()
-{
+// 初始化管道
+void pipe_init() {
     fs_register_op(FS_TYPE_PIPE, &pipe_op);
 }
